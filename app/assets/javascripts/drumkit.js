8ffc09299e58f031b3489226f378ccdb7fb6d40e
0xKubitus <n.cubilier@gmail.com>
2022-05-31 22:20:02 +0200

    sounds are working -> now it's time to make it better

--- app/assets/javascripts/drumkit.js
+++ app/assets/javascripts/drumkit.js
@@ -1,14 +1,11 @@
-// console.log("oh yeeeeaaaah"); // pour vérifier que mon fichier drumkit.js soit bien pris en compte sur chacune de mes pages.
-
 window.addEventListener('keydown', function(evnt) {
-  console.log(evnt); // on peut ajouter ".key" ou ". + n'importe quelle propriété de cet event" (les propriétés sont listées quand on toggle l'evenement dans la console du browser)
   
-  const audio = document.querySelector(`audio[data-key="${evnt.keyCode}"]`);
-  
-  if(!audio) 
-  console.log("no audio, bro!");
-  // return; // stop the function from running all together.
-  else 
+  // console.log(evnt); // on peut ajouter ".key" ou ". + n'importe quelle propriété de cet event" à 'evnt'. (les propriétés sont listées quand on toggle l'evenement dans la console du browser)
+  console.log(evnt);
+
+  const audio = document.querySelector(`audio[id="${evnt.keyCode}"]`);
+
   audio.play();
+
 
 });
