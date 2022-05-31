3fe5846f6ce08e05efd2882c90d77d7525be136e
0xKubitus <n.cubilier@gmail.com>
2022-05-31 19:22:08 +0200

    find out why there is no audio although I think I have followed exactly the indications

--- app/assets/javascripts/drumkit.js
+++ app/assets/javascripts/drumkit.js
@@ -1,7 +1,14 @@
 // console.log("oh yeeeeaaaah"); // pour vérifier que mon fichier drumkit.js soit bien pris en compte sur chacune de mes pages.
 
 window.addEventListener('keydown', function(evnt) {
-  // console.log(evnt); // on peut ajouter ".key" ou ". + n'importe quelle propriété de cet event" (les propriétés sont listées quand on toggle l'evenement dans la console du browser)
-  const audio = document.querySelector(`audio[data-key="${evnt.key}"]`);
-  console.log(audio);
+  console.log(evnt); // on peut ajouter ".key" ou ". + n'importe quelle propriété de cet event" (les propriétés sont listées quand on toggle l'evenement dans la console du browser)
+  
+  const audio = document.querySelector(`audio[data-key="${evnt.keyCode}"]`);
+  
+  if(!audio) 
+  console.log("no audio, bro!");
+  // return; // stop the function from running all together.
+  else 
+  audio.play();
+
 });
